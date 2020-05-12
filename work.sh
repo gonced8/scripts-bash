70e96deaf1576e61c137b9eaabc4ba24b7251267
Gonçalo Raposo <gonced8@gmail.com>
2020-05-12 17:16:47 +0100

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-tmux rename-window -t 0 "positnn"
-tmux send-keys -t "positnn" "cd uni/code/positnn/include/positnn" C-m
+# Mount INESC home
+#sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 inesc:/home/graposo /home/gonced8/Remote/INESC
 
-tmux new-window -n "project"
-tmux send-keys -t "project" "cd uni/code/mnist" C-m
+# This
+tmux rename-window -t 0 "this"
 
-tmux new-window -n "build"
-tmux send-keys -t "build" "cd uni/code/mnist/build" C-m
+tmux new-window -n "inesc"
+tmux send-keys -t "inesc" "ssh -XC inesc" C-m
 
-tmux new-window -n "logbook"
-tmux send-keys -t "logbook" "cd uni/logbook" C-m
-
-#tmux new-window -n "plots"
-#tmux send-keys -t "plots" "cd uni/code/pytorch_regression/plots" C-m
-
-tmux select-window -t 0
+#tmux select-window -t 0
